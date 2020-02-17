c23d41162c7d7f937c0c520036a11139c7eda257
Alexander Yemelyanov <a.yemelyanov@gmail.com>
2020-02-17 12:08:33 +0000

    ig sentiment

--- backend/update_ig_sentiment.pl
+++ backend/update_ig_sentiment.pl
@@ -13,7 +13,9 @@ my $url = "http://www.dailyfx.com/sentiment-report";
 use LWP::Simple;
 use File::Copy;
 
-chdir("/home/alex/esforexco.github.io/backend");
+chdir("/home/alex/esforexco.github.io/");
+system("git pull");
+chdir("backend");
 
 my $content = `curl --silent -L $url`;
 
@@ -27,4 +29,4 @@ my $image_name = substr($image, rindex($image,"/")+1);
 system("curl --silent -L $image > ../images/ig_sentiment/".$image_name);
 copy("../images/ig_sentiment/".$image_name,"../images/ig_sentiment/last.png");
 
-system("cd .. && git pull && git add --all && git commit -m 'ig sentiment' && git push -u origin master");
+system("cd .. && git add --all && git commit -m 'ig sentiment' && git push -u origin master");
