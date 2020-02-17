ac8442db8860c0b91f934d4f0de165d4d29a4b76
Alexander Yemelyanov <a.yemelyanov@gmail.com>
2020-02-17 06:07:11 +0000

    ig sentiment

--- backend/update_ig_sentiment.pl
+++ backend/update_ig_sentiment.pl
@@ -28,4 +28,4 @@ my $image_name = substr($image, rindex($image,"/")+1);
 system("curl --silent -L $image > ../images/ig_sentiment/".$image_name);
 copy("../images/ig_sentiment/".$image_name,"../images/ig_sentiment/last.png");
 
-system("git add --all && git commit -m 'ig sentiment' && git push -u origin master");
+system("cd .. && git add --all && git commit -m 'ig sentiment' && git push -u origin master");
